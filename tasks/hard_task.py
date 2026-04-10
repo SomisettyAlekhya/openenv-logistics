8150c7c81b62d1a82be386188a5a432e4b0ad0dc
SomisettyAlekhya <96366568+SomisettyAlekhya@users.noreply.github.com>
2026-04-10 16:32:10 +0530

    Refactor grade function parameter from env_state to state

--- tasks/hard_task.py
+++ tasks/hard_task.py
@@ -1,14 +1,14 @@
+def grade(state):
 
-def grade(env_state):
     score = 0.0
 
-    if env_state["pending_orders"] == 0:
+    if state["pending_orders"] == 0:
         score += 0.5
 
-    if env_state["traffic_level"] == 0:
+    if state["traffic_level"] == 0:
         score += 0.3
 
-    if env_state["vehicle_capacity"] > 3:
+    if state["total_delivered"] >= 10:
         score += 0.2
 
     return min(score,1.0)
