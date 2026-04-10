0bf63ec48605e803a140e576ba26a047a35c85ce
SomisettyAlekhya <96366568+SomisettyAlekhya@users.noreply.github.com>
2026-04-11 00:00:22 +0530

    Delete tasks/medium_task.py

--- tasks/medium_task.py
+++
@@ -1,8 +0,0 @@
-
-def grade(env_state):
-    score = 0.5
-    if env_state["traffic_level"] == 0:
-        score += 0.3
-    if env_state["pending_orders"] < 5:
-        score += 0.2
-    return min(score,1.0)
