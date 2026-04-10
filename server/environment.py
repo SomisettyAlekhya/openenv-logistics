538714195f6eb406c5484f9770cb733df2caa7fa
SomisettyAlekhya <96366568+SomisettyAlekhya@users.noreply.github.com>
2026-04-10 16:31:44 +0530

    Update environment.py

--- server/environment.py
+++ server/environment.py
@@ -1,4 +1,3 @@
-
 import random
 
 class LogisticsEnv:
@@ -10,35 +9,55 @@ class LogisticsEnv:
         self.pending_orders = 10
         self.vehicle_capacity = 5
         self.traffic_level = random.randint(0,2)
+        self.total_delivered = 0
+
         return self.state()
 
     def state(self):
         return {
             "pending_orders": self.pending_orders,
             "vehicle_capacity": self.vehicle_capacity,
-            "traffic_level": self.traffic_level
+            "traffic_level": self.traffic_level,
+            "total_delivered": self.total_delivered
         }
 
     def step(self, action):
 
         reward = 0.0
 
+        # Dispatch action
         if action == "dispatch":
+
             delivered = min(self.vehicle_capacity, self.pending_orders)
             self.pending_orders -= delivered
-            reward = delivered / 10
+            self.total_delivered += delivered
 
+            # delivery efficiency reward
+            reward += delivered / 10
+
+            # traffic penalty
             if self.traffic_level == 2:
                 reward *= 0.6
 
+        # Reroute action
         elif action == "reroute":
-            self.traffic_level = max(0, self.traffic_level - 1)
-            reward = 0.3
 
+            if self.traffic_level > 0:
+                self.traffic_level -= 1
+                reward += 0.3
+            else:
+                reward += 0.1
+
+        # Delay action
         elif action == "delay":
-            reward = 0.1
 
-        reward = min(max(reward,0.0),1.0)
+            reward += 0.05
+
+        # completion bonus
+        if self.pending_orders == 0:
+            reward += 0.3
+
+        reward = max(0.0, min(1.0, reward))
 
         done = self.pending_orders == 0
 
