7e0a696dfda14e1a0e829fc4a3f3dd18ac9a77f1
SomisettyAlekhya <96366568+SomisettyAlekhya@users.noreply.github.com>
2026-04-11 13:13:15 +0530

    Change return values to float in grade function
    Updated return values to be float instead of int for grading.

--- tasks/medium_reroute/grader.py
+++ tasks/medium_reroute/grader.py
@@ -3,13 +3,13 @@ def grade(output, info=None):
     Grades based on delay decision
     """
     if not output:
-        return 0
+        return 0.0
 
     output = str(output).lower()
 
     if "delay" in output:
-        return 1
+        return 1.0
     elif "wait" in output:
         return 0.5
 
-    return 0
+    return 0.0
